5609d91dfd9e18a06a9c71f3f6408ed48aff388e
Chet Murthy <chetsky@gmail.com>
2020-10-14 13:45:39 -0700

    interim checkin.  It doesn't work (yet)
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   .gitignore
    #	modified:   dune
    #	modified:   examples/ppx_curried_constr/.merlin
    #	modified:   examples/ppx_curried_constr/dune
    #	modified:   examples/ppx_overload/.merlin
    #	modified:   examples/ppx_overload/dune
    #	modified:   examples/ppx_type_of/.merlin
    #	modified:   examples/ppx_type_of/dune
    #	modified:   examples/ppx_type_of/mod.ml
    #	new file:   examples/ppx_type_of/printtyped2.ml
    #	new file:   examples/ppx_type_of/tests/.gitignore
    #	modified:   examples/ppx_type_of/tests/test.sh
    #	modified:   src/.merlin
    #	modified:   src/dune
    #

--- examples/ppx_type_of/tests/test.sh
+++ examples/ppx_type_of/tests/test.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Test only works when ppx_type_of is installed
 
-ocamlfind ocamlc -package ppx_type_of -o test_ test.ml
+ocamlfind not-ocamlfind/papr_official.exe -binary-output -impl test.ml /tmp/testXX.1
+ocamlfind ppx_type_of/ppx.exe --as-ppx -typpx-dump-first /tmp/testXX.1 /tmp/testXX.2
+ocamlfind not-ocamlfind/papr_official.exe -binary-input -impl /tmp/testXX.2
+
+exit
+
+not-ocamlfind preprocess -verbose  -package ppx_type_of test.ml
+
+ocamlfind ocamlc -verbose -package ppx_type_of -o test_ test.ml
 ./test_
